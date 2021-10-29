9fc1a5985dc6e960446678c244420a4dec6cd853
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-10-29 16:59:08 +0530

    Fixed sider issues

--- Building Web Applications in PHP/Guessing Game/guess.php
+++ Building Web Applications in PHP/Guessing Game/guess.php
@@ -6,15 +6,15 @@
 <h1>Welcome to my guessing game</h1>
 <p>
     <?php
-    if ( ! isset($_GET['guess']) ) {
+    if (! isset($_GET['guess'])) {
         echo("Missing guess parameter");
-    } else if ( strlen($_GET['guess']) < 1 ) {
+    } elseif (strlen($_GET['guess']) < 1) {
         echo("Your guess is too short");
-    } else if ( ! is_numeric($_GET['guess']) ) {
+    } elseif (! is_numeric($_GET['guess'])) {
         echo("Your guess is not a number");
-    } else if ( $_GET['guess'] < 27 ) {
+    } elseif ($_GET['guess'] < 27) {
         echo("Your guess is too low");
-    } else if ( $_GET['guess'] > 27 ) {
+    } elseif ($_GET['guess'] > 27) {
         echo("Your guess is too high");
     } else {
         echo("Congratulations - You are right");
