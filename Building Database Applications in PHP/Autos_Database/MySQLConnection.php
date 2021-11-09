50e1f6ef9c34092fa2a7aca0edd2be71a669777d
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-11-09 12:44:32 +0530

    Added the error log for database and index

--- Building Database Applications in PHP/Autos_Database/MySQLConnection.php
+++ Building Database Applications in PHP/Autos_Database/MySQLConnection.php
@@ -3,14 +3,17 @@
 class MySQLConnection
 {
     private PDO $pdo;
+
+
     public function __construct() {
         try{
-            $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc','fred', 'zap');
+            $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=roster','root', 'root');
             // See the "errors" folder for details...
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        } catch (Exception $e){
+        } catch (Exception $e) {
+            $now = new DateTime('now');
             echo "Connection Failed: " . $e->getMessage();
+            error_log($now->format('c') . " Database Not Connected: " . $e, 3, "errorLogDatabase.log");
         }
     }
 
--- Building Database Applications in PHP/Autos_Database/login.php
+++ Building Database Applications in PHP/Autos_Database/login.php
@@ -4,32 +4,67 @@ require_once "MySQLConnection.php";
 // creating the object
 $mysqlObj = new MySQLConnection();
 
+$now = new DateTime('now');
+
 
 // p' OR '1' = '1
 
+// hashing values
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';  // Pw is meow123
+
+$failure = false;
+
 if (isset($_POST['email'], $_POST['password'])) {
     echo("<p>Handling POST data...</p>\n");
-
-    $sql = "SELECT name FROM users WHERE email = :em AND password = :pw";
-
-    echo "<p>$sql</p>\n";
-
-    $stmt = $mysqlObj->getPDO()->prepare($sql);
-    $stmt->execute(array(
-        ':em' => $_POST['email'],
-        ':pw' => $_POST['password']));
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    var_dump($row);
-    
-    if ( $row === FALSE ) {
-        echo "<h1>Login incorrect.</h1>\n";
+    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+        $failure = "Email and password are required";
+        error_log($now->format('c') . " Login Fail: $failure", 3,"errorLogLogin.log");
+    } else if (str_contains($_POST['email'], "@")) {
+        $failure = "Email must have an at-sign (@)";
+        error_log($now->format('c') . " Login Fail: $failure", 3,"errorLogLogin.log");
     } else {
-        echo "<p>Login success.</p>\n";
+        $check = hash('md5', $salt.$_POST['password']);
+        if ($check === $stored_hash){
+            $sql = "SELECT name FROM users WHERE email = :em AND password = :pw";
+
+            echo "<p>$sql</p>\n";
+
+            $stmt = $mysqlObj->getPDO()->prepare($sql);
+            $stmt->execute(array(
+                ':em' => $_POST['email'],
+                ':pw' => $_POST['password']));
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            var_dump($row);
+
+            if ( $row === FALSE ) {
+                $failure = "Login Incorrect as details not in database";
+                error_log($now->format('c') . " Login Fail: $failure", 3,"errorLogLogin.log");
+            } else {
+                $failure = "Login Success";
+                error_log($now->format('c') . " Login Success: " . $_POST['email'], 3,"successLogLogin.");
+                header("Location: autos.php?name=".urlencode($_POST['email']));
+                return;
+            }
+        } else {
+            $failure = "Incorrect Password";
+            error_log($now->format('c') . " Login Fail: " . $_POST['email'] . " $check", 3,"errorLogLogin.log");
+        }
     }
+
+
 }
 ?>
 <p>Please Login</p>
+<?php
+// Note triple not equals and think how badly double
+// not equals would work here...
+if ( $failure !== false ) {
+    // Look closely at the use of single and double quotes
+    echo('<p style="color: red;">'.htmlentities($failure)."</p>\n");
+}
+?>
 <form method="post">
     <p>Email:
         <label>
