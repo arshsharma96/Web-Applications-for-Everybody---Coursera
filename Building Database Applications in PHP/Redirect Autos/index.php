e609a96a5c2e3aa4582c7f89c6673dd95488c969
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-11-11 16:15:00 +0530

    Created the add and view php files as two parts of auto.php

--- Building Database Applications in PHP/Redirect Autos/index.php
+++ Building Database Applications in PHP/Redirect Autos/index.php
@@ -18,7 +18,12 @@
     </p>
     <p>
         Attempt to go to
-        <a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+        <a href="view.php">view.php</a> without logging in - it should fail with an error message.
+    </p>
+    <p>
+        Attempt to go to
+        <a href="add.php">add.php</a> without logging in - it should fail with an error message.
+    </p>
     <p>
         <a href="https://www.wa4e.com/assn/autosdb/" target="_blank">Specification for this Application</a>
     </p>
--- Building Database Applications in PHP/Redirect Autos/view.php
+++ Building Database Applications in PHP/Redirect Autos/view.php
@@ -17,52 +17,8 @@ if (isset($_POST['logout'])){
 }
 
 if (isset($_POST['add'])){
-    $email = $_POST['email'];
-    if (isset($_POST['make'], $_POST['year'], $_POST['mileage'])){
-        if (strlen($_POST['make']) < 1){
-            $failure = "Make is required";
-            error_log($now->format('c') . " Error : $failure \n", 3,"errorLogAutos.log");
-            //$databaseStatus = "$failure";
-            header("Location: autos.php?email=".urlencode($email)."&status=".urlencode($failure));
-            return;
-        }
-
-        if((!is_numeric($_POST['year'])) || (!is_numeric($_POST['mileage']))){
-            $failure = "Mileage and year must be numeric";
-            error_log($now->format('c') . " Error : $failure \n", 3,"errorLogAutos.log");
-            //$databaseStatus = "$failure";
-            header("Location: autos.php?email=".urlencode($email)."&status=".urlencode($failure));
-            return;
-        }
-        else {
-            try{
-                $sql = "INSERT INTO autos (make, year, mileage) VALUES (:mk, :yr, :mi)";
-                $stmt = $mysqlObj->getPDO()->prepare($sql);
-                $stmt->execute(array(
-                    ':mk' => htmlentities($_POST['make']),
-                    ':yr' => htmlentities($_POST['year']),
-                    ':mi' => htmlentities($_POST['mileage'])
-                ));
-                var_dump($stmt);
-                $databaseStatus = "Record Inserted";
-                error_log($now->format('c') . " Record Inserted :" . "\n", 3,"successLogDatabase.log");
-                header("Location: autos.php?email=".urlencode($email)."&status=".urlencode($databaseStatus));
-                return;
-            } catch (Exception $e){
-                $databaseStatus = "Record not inserted due to error";
-                error_log($now->format('c') . " Error : " . $e, 3, "errorLogDatabase.log");
-                header("Location: autos.php?email=".urlencode($email)."&status=".urlencode($databaseStatus));
-                return;
-            }
-
-        }
-    } else {
-        $failure = "All the fields must be filled";
-        $databaseStatus = "Record not inserted due to error";
-        error_log($now->format('c') . " Error : $failure \n", 3,"errorLogAutos.log");
-        header("Location: autos.php?email=".urlencode($email)."&status=".urlencode($databaseStatus));
-        return;
-    }
+    header("Location: add.php");
+    return;
 }
 
 
@@ -94,13 +50,18 @@ if(isset($_SESSION['name'])){
 
 <html lang="en">
 <head>
-    <title>Autos Page</title>
+    <title>Autos View Page</title>
 </head>
 <body>
-<h1>Tracking Autos for <?php echo htmlentities($_GET['email']); ?> </h1>
+<h1>Tracking Autos for <?php echo htmlentities($_SESSION['name']); ?> </h1>
+<?php
+if ( isset($_SESSION['success']) ) {
+    echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
+    unset($_SESSION['success']);
+}
+?>
 <p><h3>Automobiles</h3>
 <?php
-if(!empty($_GET['status'])){
     $stmt = $mysqlObj->getPDO()->query("SELECT make, year, mileage FROM autos");
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo "<ul>";
@@ -109,13 +70,9 @@ if(!empty($_GET['status'])){
         echo($row["make"] . " " . $row["year"] . " / " . $row["mileage"]);
         echo("</li>");
     }
-    echo "</table>\n";
-}
+    echo "</ul>\n";
 ?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <label>
-        <input type="hidden" name="email" value="<?php echo $_GET['email'] ?>"
-    </label>
     <input type="submit" name="add" value="Add New"/>&nbsp;&nbsp;&nbsp;
     <input type="submit" name="logout" value="Logout"/>&nbsp;
 </form>
